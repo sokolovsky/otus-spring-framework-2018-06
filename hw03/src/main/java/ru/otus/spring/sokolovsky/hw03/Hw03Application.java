1bf2a3617f77ae9a062cebbd16de75989f065f6b
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-07-14 18:22:44 +0300

    HW03. The main process of program.

--- hw03/src/main/java/ru/otus/spring/sokolovsky/hw03/Hw03Application.java
+++ hw03/src/main/java/ru/otus/spring/sokolovsky/hw03/Hw03Application.java
@@ -2,11 +2,18 @@ package ru.otus.spring.sokolovsky.hw03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.otus.spring.sokolovsky.hw03.domain.QuizIterator;
+import ru.otus.spring.sokolovsky.hw03.presentation.QuizPresentation;
 
 @SpringBootApplication
 public class Hw03Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Hw03Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Hw03Application.class, args);
+        QuizPresentation quizPresentation = context.getBean(QuizPresentation.class);
+        QuizIterator quizIterator = context.getBean(QuizIterator.class);
+
+        quizPresentation.run(quizIterator);
 	}
 }
