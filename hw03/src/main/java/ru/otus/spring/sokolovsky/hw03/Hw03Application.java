4dab16d8066e79e1fad44bb8830180e57dfc217d
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-07-14 17:21:02 +0300

    HW03. Spring boot inited

---
+++ hw03/src/main/java/ru/otus/spring/sokolovsky/hw03/Hw03Application.java
@@ -0,0 +1,12 @@
+package ru.otus.spring.sokolovsky.hw03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw03Application.class, args);
+	}
+}
