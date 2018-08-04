555a7321b7cb8add2f28f526d8e5eaf00adf2780
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-08-04 16:39:52 +0300

    HW07. The first adjustment of application after applying Spring Initializer.

---
+++ hw07/src/main/java/ru/otus/spring/sokolovsky/hw07/Hw07Application.java
@@ -0,0 +1,12 @@
+package ru.otus.spring.sokolovsky.hw07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw07Application.class, args);
+    }
+}
