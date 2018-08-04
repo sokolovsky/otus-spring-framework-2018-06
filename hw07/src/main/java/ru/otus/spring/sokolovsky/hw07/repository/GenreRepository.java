5cbcfa65f5e49fd5ba2d0ff198cc09224855b2f8
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-08-04 17:32:13 +0300

    HW07. Add service layer into application.

--- hw07/src/main/java/ru/otus/spring/sokolovsky/hw07/repository/GenreRepository.java
+++ hw07/src/main/java/ru/otus/spring/sokolovsky/hw07/repository/GenreRepository.java
@@ -4,8 +4,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.otus.spring.sokolovsky.hw07.domain.Genre;
 
+import java.util.List;
+
 @Repository
 public interface GenreRepository extends CrudRepository<Genre, Long> {
+
+    Genre findById(long id);
+
+    List<Genre> findAll();
 
     Genre findByTitle(String s);
 }
