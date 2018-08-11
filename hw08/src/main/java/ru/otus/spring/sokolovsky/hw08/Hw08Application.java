31eeb113cfe4e35a4b8990bea26bb53dc31ea90c
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-08-11 14:27:45 +0300

    HW08. After spring initializer work.

---
+++ hw08/src/main/java/ru/otus/spring/sokolovsky/hw08/Hw08Application.java
@@ -0,0 +1,12 @@
+package ru.otus.spring.sokolovsky.hw08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw08Application.class, args);
+    }
+}
