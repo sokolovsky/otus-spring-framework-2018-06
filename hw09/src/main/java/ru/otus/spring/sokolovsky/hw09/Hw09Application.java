ba45a8f34863b218eb53090be6cd5a4a01760f14
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-08-18 12:32:35 +0300

    HW09. Project initialization.

---
+++ hw09/src/main/java/ru/otus/spring/sokolovsky/hw09/Hw09Application.java
@@ -0,0 +1,12 @@
+package ru.otus.spring.sokolovsky.hw09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw09Application.class, args);
+    }
+}
