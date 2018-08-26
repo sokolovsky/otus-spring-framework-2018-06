37eed9dd78e6317bccc354ba6d2893245fa52665
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-08-26 19:00:25 +0300

    HW10. After project initializer.

---
+++ hw10/src/main/java/ru/otus/spring/sokolovsky/hw10/Hw10Application.java
@@ -0,0 +1,12 @@
+package ru.otus.spring.sokolovsky.hw10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw10Application.class, args);
+	}
+}
